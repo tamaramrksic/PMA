36f6a5abf2261a7a6221b1cdc9c3059877811b5f
Tamara <tamarica94mrksic@gmail.com>
2018-04-07 18:33:33 +0200

    Implemented GUI for login. #1

---
+++ app/src/main/java/com/example/ftn/showbook/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.ftn.showbook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+        TextView clickableTextLink = (TextView)findViewById(R.id.registration);
+        clickableTextLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
