b519346f37e9e7f35bcc9e79c87d31216a1427e4
Ana <1994.ana@gmail.com>
2018-04-07 21:01:18 +0200

    Implemented GUI for user registration. #2

--- app/src/main/java/com/example/ftn/showbook/RegisterActivity.java
+++ app/src/main/java/com/example/ftn/showbook/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.ftn.showbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView clickableTextLink = (TextView)findViewById(R.id.login);
+        clickableTextLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
