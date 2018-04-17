62a57eac647d8fda0344c71ca2a3a960812edc6c
Jelena K <jelenakalabic@gmail.com>
2018-04-17 18:51:01 +0200

    Showing details for chosen list item. #14

--- app/src/main/java/com/example/ftn/showbook/ReservationListAdapter.java
+++ app/src/main/java/com/example/ftn/showbook/ReservationListAdapter.java
@@ -2,6 +2,7 @@ package com.example.ftn.showbook;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +30,15 @@ public class ReservationListAdapter extends RecyclerView.Adapter<ReservationList
             date = (TextView) view.findViewById(R.id.date_value);
             time = (TextView) view.findViewById(R.id.time_value);
             rating = (TextView) view.findViewById(R.id.show_reserved_rating);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, ReservedShowDetailsActivity.class);
+                    context.startActivity(intent);
+                }
+            });
 
         }
     }
--- app/src/main/java/com/example/ftn/showbook/ShowListAdapter.java
+++ app/src/main/java/com/example/ftn/showbook/ShowListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.ftn.showbook;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +29,15 @@ public class ShowListAdapter extends RecyclerView.Adapter<ShowListAdapter.ViewHo
             description = (TextView) view.findViewById(R.id.show_repertoire_description);
             duration = (TextView) view.findViewById(R.id.show_repertoire_duration);
             rating = (TextView) view.findViewById(R.id.show_repertoire_rating);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, ShowDetailsActivity.class);
+                    context.startActivity(intent);
+                }
+            });
 
         }
     }
